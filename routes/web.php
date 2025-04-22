2a20b5ade56d7dd39b95e6feec8ce57c638ac17f
DevJourney19 <F4r3ver@gmail.com>
2025-04-22 15:07:57 -0500

    feat: Se realizó un navbar básico.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    //Corregir (cambiar) para evitar llamar al fragmento layout.navbar
+    return view('layout.navbar');
 });
