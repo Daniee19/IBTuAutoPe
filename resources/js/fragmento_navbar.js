c590c9d16beec487a406998050bf5bfca78350b5
DevJourney19 <F4r3ver@gmail.com>
2025-04-28 17:57:21 -0500

    feat: Se mejoró la responsividad de la portada.

--- resources/js/fragmento_navbar.js
+++ resources/js/fragmento_navbar.js
@@ -1,10 +1,15 @@
 
 let elemento = document.querySelector(".iconito_hamburguesa");
 
+//Agregar blur a los elementos exteriores al slide bar
+let div_blur = document.getElementById("overlay_blur");
 
 elemento.addEventListener("click", function () {
     let div = document.querySelector(".lista");
     div.classList.toggle("aparecer");
+
+
+    div_blur.classList.add("agregar_blur");
 })
 
 
@@ -14,4 +19,5 @@ x.addEventListener("click", function () {
 
     let panel = document.querySelector(".lista");
     panel.classList.remove("aparecer");
+    div_blur.classList.remove("agregar_blur");
 })
